3cd08ec4ef4547ee450f0a083dcfec5b35847adc
Amadeus710 <69098664+Amadeus710@users.noreply.github.com>
2022-06-14 19:05:30 -0400

    make home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import { Topbar } from '../../components/Topbar/Topbar'
+import React from "react";
+import "./Home.css";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import TimeLine from "../../components/timeline/TimeLine";
+import { Topbar } from "../../components/Topbar/Topbar";
 
 export default function Home() {
-  return (
-      <>
-          <Topbar />
-      </>
-  )
+    return (
+        <>
+            <Topbar />
+            <div className='homeContainer'>
+                <Sidebar />
+                <TimeLine />
+                <Rightbar />
+            </div>
+        </>
+    );
 }
